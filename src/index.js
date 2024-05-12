d4326f3d6da6eb3d06b4d5e22c469503f4625663
caleb <caleb.peterson@cubicle6.com>
2024-05-12 19:07:24 -0400

    add CLI --help options

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ const { Webview } = require("webview-nodejs");
 const minimist = require("minimist");
 
 const argv = minimist(process.argv.slice(2));
-const domain = argv.domain ? `${argv.domain}/` : "";
+
+if (argv.help) {
+  console.log("Usage:");
+  console.log("");
+  console.log("  devdocs <query>");
+  console.log("  devdocs --api <node|css|html|http> <query>");
+  console.log("");
+  process.exit(0);
+}
+
+const api = argv.api ? `${argv.api}/` : "";
 const query = encodeURIComponent(argv._.join(" "));
 
 const w = new Webview(true);
 w.title("DevDocs");
 w.size(1280, 1024);
 
-w.navigate(`https://devdocs.io/${domain}#q=${query}`);
+w.navigate(`https://devdocs.io/${api}#q=${query}`);
 
 w.show();
